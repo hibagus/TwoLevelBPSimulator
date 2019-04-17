75bb23be642dc929009399b90e07df8871d60553
hibagus <git@bagus.my.id>
2019-04-16 23:45:38 -0500

    initial implementation of trace entry

---
+++ inc/trace_entry.hpp
@@ -0,0 +1,24 @@
+/*
+Hanin Two-Level Branch Predictor Simulator
+EE382N - Superscalar Microprocessor Architecture
+Spring 2019 - Assignment 4
+================================================
+Copyright (C)2019 Bagus Hanindhito (bh29293)
+hanindhito[at]bagus[dot]my[dot]id 
+http://www.bagus.my.id
+*/
+
+#ifndef TRACE_ENTRY_H
+#define TRACE_ENTRY_H
+
+class trace_entry
+{
+  unsigned int branch_addr ;
+  bool         branch_outcome;
+
+public:
+  trace_entry(unsigned int branch_addr, bool branch_outcome);
+  unsigned int getBranchAddr();
+  bool getBranchOutcome();
+};
+#endif
---
+++ src/trace_entry.cpp
@@ -0,0 +1,27 @@
+/*
+Hanin Two-Level Branch Predictor Simulator
+EE382N - Superscalar Microprocessor Architecture
+Spring 2019 - Assignment 4
+================================================
+Copyright (C)2019 Bagus Hanindhito (bh29293)
+hanindhito[at]bagus[dot]my[dot]id 
+http://www.bagus.my.id
+*/
+
+#include "trace_entry.hpp"
+
+trace_entry::trace_entry(unsigned int branch_addr, bool branch_outcome)
+{
+  branch_addr    = branch_addr;
+  branch_outcome = branch_outcome;
+}
+
+unsigned int trace_entry::getBranchAddr()
+{
+	return branch_addr;
+}
+
+bool trace_entry::getBranchOutcome()
+{
+	return branch_outcome;
+}
