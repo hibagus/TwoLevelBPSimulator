41f97141e8d70413dfe1a019cda9c0364f817493
hibagus <git@bagus.my.id>
2019-04-20 00:44:23 -0500

    initial implementation

---
+++ inc/branch_history_register.hpp
@@ -0,0 +1,36 @@
+/*
+Hanin Two-Level Branch Predictor Simulator
+EE382N - Superscalar Microprocessor Architecture
+Spring 2019 - Assignment 4
+================================================
+Copyright (C)2019 Bagus Hanindhito (bh29293)
+hanindhito[at]bagus[dot]my[dot]id 
+http://www.bagus.my.id
+*/
+
+#ifndef BRANCH_HISTORY_REGISTER_H
+#define BRANCH_HISTORY_REGISTER_H
+
+class branch_history_register
+{
+public:
+    branch_history_register();
+    branch_history_register(unsigned int length);
+	branch_history_register(unsigned int initial, unsigned int length);
+	branch_history_register(unsigned long long initial, unsigned int length);
+    void updateTaken();
+	void updateNotTaken();
+	void reset();
+	void reset(unsigned int initial);
+	void reset(unsigned long long initial);
+	unsigned long long getLongValue();
+	unsigned int getIntValue();
+	unsigned int getLength();
+	void printBinary();
+	void printHex();
+private:
+    unsigned long long _value;
+	unsigned int _length;
+	void _truncateMSB();
+};
+#endif
---
+++ src/branch_history_register.cpp
@@ -0,0 +1,123 @@
+/*
+Hanin Two-Level Branch Predictor Simulator
+EE382N - Superscalar Microprocessor Architecture
+Spring 2019 - Assignment 4
+================================================
+Copyright (C)2019 Bagus Hanindhito (bh29293)
+hanindhito[at]bagus[dot]my[dot]id 
+http://www.bagus.my.id
+*/
+
+#include "branch_history_register.hpp"
+#include <bitset>
+#include <iostream>
+#include <string>
+
+
+branch_history_register::branch_history_register()
+{
+	this->_value = 0;
+	this->_length= 1;
+}
+
+branch_history_register::branch_history_register(unsigned int length)
+{
+	this->_value = 0;
+	if(length==0)
+	{
+		this->_length= 1;
+	}
+	else
+	{
+		this->_length= length;
+	}
+	
+}
+
+branch_history_register::branch_history_register(unsigned int initial, unsigned int length)
+{
+	this->_value = initial;
+	if(length==0)
+	{
+		this->_length= 1;
+	}
+	else
+	{
+		this->_length= length;
+	}
+	this->_truncateMSB();
+}
+
+branch_history_register::branch_history_register(unsigned long long initial, unsigned int length)
+{
+	this->_value = initial;
+	if(length==0)
+	{
+		this->_length= 1;
+	}
+	else
+	{
+		this->_length= length;
+	}
+	this->_truncateMSB();
+}
+
+void branch_history_register::updateTaken()
+{
+	this->_value = (this->_value << 1) | 0x01;
+	this->_truncateMSB();
+}
+
+void branch_history_register::updateNotTaken()
+{
+	this->_value = (this->_value << 1) | 0x00;
+	this->_truncateMSB();
+}
+
+void branch_history_register::reset()
+{
+	this->_value = 0;
+}
+
+void branch_history_register::reset(unsigned int initial)
+{
+	this->_value = initial;
+	this->_truncateMSB();
+}
+
+void branch_history_register::reset(unsigned long long initial)
+{
+	this->_value = initial;
+	this->_truncateMSB();
+}
+
+unsigned long long branch_history_register::getLongValue()
+{
+	return this->_value;
+}
+
+unsigned int branch_history_register::getIntValue()
+{
+	unsigned int mask = 0;
+	mask = ~mask;
+	unsigned int truncatedvalue = this->_value & mask;
+	return truncatedvalue;
+}
+
+unsigned int branch_history_register::getLength()
+{
+	return this->_length;
+}
+
+void branch_history_register::printBinary()
+{
+	std::string bitstring = std::bitset<64>(this->_value).to_string();
+	std::cout << bitstring.substr(64-this->_length,63);
+}
+
+void branch_history_register::_truncateMSB()
+{
+	unsigned long long mask = 0;
+	mask = ~((~mask)<<this->_length);
+	this->_value = _value & mask;
+}
