e40d192d40342aaf6329cd272bc3bffdc908013c
hibagus <git@bagus.my.id>
2019-04-22 10:05:01 -0500

    add header

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,13 @@
+/*
+Hanin Two-Level Branch Predictor Simulator
+EE382N - Superscalar Microprocessor Architecture
+Spring 2019 - Assignment 4
+================================================
+Copyright (C)2019 Bagus Hanindhito (bh29293)
+hanindhito[at]bagus[dot]my[dot]id 
+http://www.bagus.my.id
+*/
+
 #include <iostream>
 #include "saturating_counter.hpp"
 #include "branch_history_table.hpp"
